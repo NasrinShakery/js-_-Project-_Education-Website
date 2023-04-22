732896102b613d34823d04412c65a5276d800b9b
nansiiii <nasrin.shakery8@gmail.com>
2023-04-22 16:33:10 +0330

    Digital Clock

--- js/app.js
+++ js/app.js
@@ -1,5 +1,35 @@
 const $ = document;
 
+function showTime(){
+  var date = new Date();
+  var h = date.getHours(); // 0 - 23
+  var m = date.getMinutes(); // 0 - 59
+  var s = date.getSeconds(); // 0 - 59
+  var session = "AM";
+  
+  if(h == 0){
+      h = 12;
+  }
+  
+  if(h > 12){
+      h = h - 12;
+      session = "PM";
+  }
+  
+  h = (h < 10) ? "0" + h : h;
+  m = (m < 10) ? "0" + m : m;
+  s = (s < 10) ? "0" + s : s;
+  
+  var time =  h + ":" + m + ":" + s +  session;
+  document.getElementById("MyClockDisplay").innerText = time;
+  document.getElementById("MyClockDisplay").textContent = time;
+  
+  setTimeout(showTime, 1000);
+  
+}
+
+// showTime();
+
 // let online = $.querySelector('#online')
 // function darkLight(){
 //   let color = document.documentElement.style.getPropertyValue(`${$violet0}`)
@@ -20,6 +50,8 @@ switchElem.addEventListener("click", function () {
 });
 //----------------------
 document.addEventListener("DOMContentLoaded", function () {
+  showTime();
+
   fetch("../json/x.json")
     .then((res) => res.json())
     .then((res) => {
@@ -203,3 +235,5 @@ var swiper = new Swiper(".reviews-slider", {
 //------------------------------
 
 // export{switchElem}
+
+
