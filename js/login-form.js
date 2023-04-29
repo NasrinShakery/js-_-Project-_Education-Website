9d907d2171c73909db8f66a8e7d584bca5f49fe8
nansiiii <nasrin.shakery8@gmail.com>
2023-04-29 17:08:02 +0330

    formValidation1

--- js/login-form.js
+++ js/login-form.js
@@ -68,3 +68,63 @@ menuBtn.onclick = () => {
   menuBtn.classList.toggle("fa-times");
   navbar.classList.toggle("active");
 };
+//---------------------------------------------------
+const formSignup = $.querySelector('#form-signup');
+const errorMessage = $.querySelector('#errorMessage');
+class FormValidator {
+  constructor(form){
+    this.__myForm = form;
+    this.submitHandler();
+    this.focusHandler();
+  }
+  submitHandler(){
+    this.__myForm.addEventListener('submit', (event) => {
+      event.preventDefault();
+      // console.log(event.target);
+
+    })
+  }
+  focusHandler(){
+    this.__myForm.addEventListener('focusin', (event) =>{
+      // console.log('focusin:');
+      // console.log(event.target);
+    })
+    this.__myForm.addEventListener('focusout', (event) => {
+      let message = this.notEmpty(event.target);
+      errorMessage.innerHTML = message;
+      if(message){
+        errorMessage.style.display = 'block';
+      }else{
+        errorMessage.style.display = 'none';
+      }
+    })
+  }
+  notEmpty(element){
+    if(element.value === ''){
+      return 'فیلدی را خالی نگذارید';
+    }else{
+      return '';
+    }
+  }
+  isPhoneNumber(element){
+    const re = '';
+    if(!re.test(element.value)){
+      return 'قالب ایمیل نادرست است';
+    }else{
+      return '';
+    }
+  }
+  isPassword(element){
+    const re = new RegExp("^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$\%^&\*])(?=.*{8,})");
+    return re.test(element.value);
+  }
+  isConfirmPassword(pass, repass){
+    if(pass !== repass){
+      return false;
+    }else{
+      return true;
+    }
+  }
+}
+
+const formVal = new FormValidator(formSignup)
